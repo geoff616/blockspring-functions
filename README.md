6382669500a0ef4ec1f14c0d115dc9d492d31652
Geoffrey Wright <Geoff@MacBook-Pro.local>
2015-08-01 22:44:54 -0400

    indexing documents in Elasticsearch https://open.blockspring.com/geoff616/90e79616e423225c856b8b8fd994942f

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# blockspring-functions
+##### Blockspring is a spreadsheet plugin that lets you create new functions in your favorite programming language. This repo contains the following functions:
+
+
+- __Write Table To Elasticsearch__: Indexes each row as a document in elasticsearch
---
+++ functions/write_table_to_elasticsearch.py
@@ -0,0 +1,71 @@
+###### Write Table To Elasticsearch
+###### This function takes a rectangular selection with a column header and will index each row as a document in elasticsearch
+######
+###### Arguments:
+######     address: Location and port of the Elasticsearch instance - 127.0.0.1:9300
+######     index: The Elasticsearch Index to write to - ecommerce_store_data
+######     type: The type of document you are indexing - monthly revenue by product category
+######     documets: Column header plus the rows to index - A1:J4
+
+import blockspring
+from datetime import datetime
+import requests
+from json import dumps
+import time
+
+## Helper funtion that takes a string like "This one" and returns "this_one"
+def to_es_friendly_string(string):
+    lower_case = string.lower()
+    no_spaces = lower_case.replace(' ', '_')
+    return no_spaces
+
+
+# Main function registered to blockspring that does the magic
+def write_table_to_es(request, response):  
+
+    ## Where is ES?
+    address = request.params['address']
+
+    ## Clean up index and doc type to use for writes
+    index = to_es_friendly_string(request.params['index'])
+    datatype = to_es_friendly_string(request.params['type'])
+
+    ## An array of arrays where the first element in the outer array is the column headers: [["Product Type", "Sales"], ["Red Shoes", 87], ...]
+    rows = request.params['documents']
+
+    ## Get the set of properties to use for each document and remove that row from the rows array
+    columns = rows.pop(0)
+    properties = [to_es_friendly_string(prop) for prop in columns]
+
+    ## Array to track if writes succeeded
+    ## TODO: Better error handling
+    write_status = []
+
+    ## Index each row as a document
+    for row in rows:
+        ## Create a dictionary with the clean labels to index
+        doc = dict(zip(properties, row))
+
+        ## Adding timestamp to the document
+        doc['indexed_at'] = str(int(datetime.now().strftime("%s")))
+
+        ## Build the URL and index the dictionary as a JSON document in elasticsearch
+        url = address + '/' + index + '/' + datatype + '/'
+        json_doc = dumps(doc)
+        req = requests.post(url, json_doc)
+
+        ## Keep track of which writes were sucessful 
+
+        if req.ok == True:
+             write_status.append('True')
+        else:
+            write_status.append('ERROR')
+
+        ##Add array of statuses to response 
+        response.addOutput("Indexed", write_status)
+
+    ## Return the response
+    response.end()
+
+## Defining write_to_es function for blockspring
+blockspring.define(write_table_to_es)
