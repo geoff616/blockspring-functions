45b1971dd0a507ecdbe29a8c84ba1d6b1539ac28
Geoffrey Wright <Geoff@MacBook-Pro.fios-router.home>
2015-08-13 14:29:08 -0400

    testing gitspring

---
+++ functions/test-block/block.py
@@ -0,0 +1,10 @@
+import blockspring
+
+def block(request, response):
+	name = "Hi! My name is " + request.params["first_name"]
+	age = " and my age is " + str(request.params["age"])
+
+	response.addOutput("intro", name + age)
+	response.end()
+
+blockspring.define(block)
