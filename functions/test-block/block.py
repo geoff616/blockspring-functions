d29d2b6f159ce28c92552e9f4510e63f099859ab
Geoffrey Wright <Geoff@MacBook-Pro.fios-router.home>
2015-08-13 14:57:13 -0400

    testing gitspring

--- functions/test-block/block.py
+++ functions/test-block/block.py
@@ -1,7 +1,7 @@
 import blockspring
 
 def block(request, response):
-	name = "Hi! My name is " + request.params["first_name"]
+	name = "Hi My name is " + request.params["first_name"]
 	age = " And my age is " + str(request.params["age"])
 
 	response.addOutput("intro", name + age)
