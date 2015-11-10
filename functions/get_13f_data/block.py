2c6b04a5e7f68366f8ffc50a94f291e1f199a06e
Geoffrey Wright <Geoff@MacBook-Pro-2.local>
2015-11-10 17:17:31 -0500

    parsing holdings into table

--- functions/get_13f_data/block.py
+++ functions/get_13f_data/block.py
@@ -29,9 +29,39 @@ def get_13f_data(request, response):
     query = query_jive(cik)
 
     results = loads(query)['_results_']
-    to_return = []
+
+    ## need to turn a nested JSON into array of arrays 
+    first_key = 'date'
+
+    ## interesting data is in _holdings_ which is an array of dicts with the keys listed
+    inner_keys = {
+        '_cover_': ['city', 'zip', 'street1', 'street2', 'state', 'manager'], 
+        '_signature_': ['city', 'state', 'title', 'name', 'phone'], 
+        '_holdings_': ['cusip', 'name', 'ticker', 'shares', 'value', 'type', 'class'], 
+        '_summary_': ['total_value', 'others', 'entries'] 
+    }
+
+
+    columns = [first_key] + inner_keys['_holdings_']
+    
+    #start building return array with column headers as first row
+    to_return = [columns]
+
+    #add a row for each holding
     for date in results.keys():
-        to_return.append([date, str(results[date])])
+        holdings = results[date]['_signature_']
+        for holding in holdings:
+            row = []
+            row.append(date)
+            #add date to first column in row
+            keys_to_check = columns[1:]
+            for key in keys_to_check:
+                if key in holding:
+                    row.append(holding[key])
+                else:
+                    row.append(0)
+
+            to_return.append(row)
 
 
     response.addOutput("results", to_return)
