ee9cee241c669be27ca95a2835c3ecdf77f69a99
Geoffrey Wright <Geoff@MacBook-Pro-2.local>
2015-11-11 14:17:01 -0500

    local change 2

--- functions/test-block/block.py
+++ functions/test-block/block.py
@@ -3,7 +3,7 @@ import blockspring
 def block(request, response):
 	to_return = [[1],[2],[3],[4]]
 
-    local_change = 1
+    local_change = 2
 
 	response.addOutput("array", to_return)
 	response.end()
